eea4ed2b665346bf0f9e731eef8fdd15b1c04f2d
blink <blindcollective@protonmail.com>
2024-11-30 13:16:29 -0500

    replaced solved.sh with util.py. This will act as my main utility script for the coming future.

---
+++ util/util.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0
+# Copyright (C) 2024 Josh Hayden <blindcollective@protonmail.com>
+"""------------------------------------------------------------
+Filename: util.py
+Author: Josh Hayden
+Description: The main utility program for codeforces-problems. 
+             A short list of commands to better analyze my solutions.
+Usage: python util.py command --flags
+-----------------------------------------------------------"""
+
+# main function definition
+def main():
+
+    return 0
+
+# main function call
+if __name__ == "__main__":
+    main()
